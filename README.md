d0430545215cbe7f029ee8963b435262dc16693a
Luj8n <l.milieska@gmail.com>
2021-08-16 15:00:52 +0300

    refactored code & more features

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 
 ### Warning: begginer rust code
 
-## Installation
+## How to run it
 
 1. Make sure that you have installed rust with [rustup](https://rustup.rs/)
 1. Add a `.env` file and fill it with your own data
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,30 @@
 use dotenv;
-use regex;
-use twitch_irc::login::StaticLoginCredentials;
-use twitch_irc::message::ServerMessage;
-use twitch_irc::ClientConfig;
-use twitch_irc::SecureTCPTransport;
-use twitch_irc::TwitchIRCClient;
+use twitch_irc::{
+  login::StaticLoginCredentials, message::ServerMessage, ClientConfig, SecureTCPTransport,
+  TwitchIRCClient,
+};
+
+fn get_env_var(name: &str) -> String {
+  return dotenv::var(name).expect(&format!("Couldn't find {} variable in the .env file", name));
+}
 
 #[tokio::main]
 pub async fn main() {
-  let login_name = dotenv::var("USERNAME").expect("No USERNAME variable in .env");
-  let oauth_token = dotenv::var("OAUTH_TOKEN").expect("No OAUTH_TOKEN variable in .env");
-
-  let config = ClientConfig::new_simple(StaticLoginCredentials::new(login_name.to_owned(), Some(oauth_token)));
-
-  let (mut incoming_messages, client) = TwitchIRCClient::<SecureTCPTransport, StaticLoginCredentials>::new(config);
-
-  let channels: Vec<_> = dotenv::var("CHANNELS")
-    .expect("No CHANNELS variable in .env")
+  let login_name = get_env_var("USERNAME");
+  let oauth_token = get_env_var("OAUTH_TOKEN");
+  let channels: Vec<_> = get_env_var("CHANNELS")
     .split(',')
-    .map(|s| s.to_string())
+    .map(|channel| channel.to_string().to_lowercase())
     .collect();
+  let bot_prefix = get_env_var("BOT_PREFIX");
+
+  let config = ClientConfig::new_simple(StaticLoginCredentials::new(
+    login_name.to_owned(),
+    Some(oauth_token),
+  ));
+
+  let (mut incoming_messages, client) =
+    TwitchIRCClient::<SecureTCPTransport, StaticLoginCredentials>::new(config);
 
   for channel in channels {
     client.join(channel)
@@ -29,30 +34,47 @@ pub async fn main() {
     while let Some(message) = incoming_messages.recv().await {
       match message {
         ServerMessage::Privmsg(msg) => {
-          println!("(#{}) {}: {}", msg.channel_login, msg.sender.name, msg.message_text);
+          println!(
+            "#{} -> {}: {}",
+            msg.channel_login, msg.sender.name, msg.message_text
+          );
 
-          if !msg.message_text.starts_with("!") || (&msg.sender.name).to_lowercase() == (&login_name).to_lowercase() {
+          if !msg.message_text.starts_with(&bot_prefix) {
             continue;
           }
 
-          let re = regex::Regex::new(r"\w+").expect("Bad regex");
-          let words: Vec<_> = re.find_iter(&msg.message_text[1..]).collect();
+          let sender_is_mod = (&msg.badges)
+            .into_iter()
+            .any(|badge| vec!["moderator", "broadcaster"].contains(&&badge.name[..]));
 
-          match words.get(0) {
-            None => {}
-            Some(first_match) => {
-              let command = first_match.as_str().to_lowercase();
-              println!("{}", command);
+          let words: Vec<_> = msg.message_text[1..]
+            .split_whitespace()
+            .map(str::to_string)
+            .collect();
 
-              match &command[..] {
-                "ping" => {
-                  client
-                    .say(msg.channel_login.to_owned(), "Pong!".to_owned())
-                    .await
-                    .expect("oof");
-                }
-                _ => {}
+          if let Some(first_word) = words.get(0) {
+            let command = first_word.to_lowercase();
+
+            println!("* got command '{}'", command);
+
+            match &command[..] {
+              "ping" => {
+                client
+                  .say(msg.channel_login.to_owned(), "Pong!".to_owned())
+                  .await
+                  .unwrap();
               }
+              "say" if sender_is_mod => {
+                if let None = words.get(1) {
+                  continue;
+                }
+
+                client
+                  .say(msg.channel_login.to_owned(), words[1..].join(" "))
+                  .await
+                  .unwrap();
+              }
+              _ => {}
             }
           }
         }
